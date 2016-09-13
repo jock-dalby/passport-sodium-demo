62866d59861fc8e4b987517155f2f2db94af3570
Martin Adams <email": null>
2016-09-14 11:37:28 +1200

    Pseudocoded login/register sequence

--- notes.js
+++ notes.js
@@ -1,6 +1,5 @@
 var express = require('express')
 
-
 var app = express()
 
 app.use(function (req, res, next) {
@@ -14,34 +13,41 @@ app.use(function (req, res, next) {
 
 function intercept (req, res, next) {
   console.log('my intercept')
-  req.user = 'Simon' 
+  req.user = 'Simon'
   next()
-
 }
-// serialise
-// deserialise
-
 
 app.get('/', intercept, function (req, res, next) {
-
   console.log('next in the chain', req.user)
   res.send('it works. Main route')
-
 })
 
 app.listen(3001, function () {
   console.log('listening')
 })
 
+//  1. FUNCTION GETTOKEN
+//  if (token) {goToAuthentication} else {next()}
 
-// 
+//  2. APP GET
+//  app.get('/', getToken, function)
+//  Upon landing on / page, sends user to getToken() function.
+//  if (!token) {res.render 'login-register'}
 
-// 1. middleware
-// 2. token ticket
-// 3. cryptographic hashing [x]
-
-// Hashing one way conversion string -> hash
-
-
-
-
+//  3. APP POST
+//    verify fields
+//    if username exists:
+//      hash password
+//      authenticate
+//      if hashed password same as in db:
+//        login user
+//        send token
+//      else:
+//        indicate error on page
+//      end if
+//    else:
+//      hash password
+//      send username and password to database
+//      login user
+//      send token
+//    end if
