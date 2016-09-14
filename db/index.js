ad6df1eb66e9c475edc27267312223a96756ac9b
Jonathan Dalby <jock-dalby@jock-dalby.local>
2016-09-14 14:38:55 +1200

    delete branch

--- db/index.js
+++
@@ -1,6 +0,0 @@
-var Knex = require('knex')
-// by default we use the devlopment config in the knexfile
-// in production knex will be configured to use postgresql
-var config = require('../knexfile')[process.env.NODE_ENV || 'development']
-
-module.exports = Knex(config)
--- features/support/hooks.js
+++ features/support/hooks.js
@@ -1,29 +1,20 @@
+require('babel-register')
 var config = require('../../config')
 var app = require('../../app')
-var knex = require('../../db')
-var knexConfig = {directory: './db/migrations'}
-
 var server
 
 module.exports = function () {
 
   this.registerHandler('BeforeFeatures', (features, callback) => {
-    knex.migrate.latest()
-    .then(function() {
-      console.log('server starting....')
+    console.log('server starting....')
     server = app.listen(config.proxy.port, () => {
       callback()
     })
   })
-})
 
   this.registerHandler('AfterFeatures', (features, callback) => {
-    knex.migrate.rollback(knexConfig)
-    .then(function () {
-        console.log('server stopping....')
-        server.close()
-        callback()
+    console.log('server stopping....')
+    server.close()
+    callback()
   })
-})
-
 }
